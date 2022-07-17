f6399324946f3589561a461a871c282b807be077
mrohmer <rohmer.matze@gmail.com>
2022-07-17 19:50:37 +0200

    fix: fix icons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,10 +5,6 @@
 
 <svelte:head>
     <title>Karolin Rohmer</title>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-    <link rel="manifest" href="/manifest.json">
 </svelte:head>
 
 <main class="z-10 min-h-screen bg-white dark:bg-zinc-900">
