134632d197a36305a1386d125cff761c4fd41b77
mrohmer <rohmer.matze@gmail.com>
2022-11-16 20:43:28 +0100

    feat: add legal pages

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,13 @@
-<footer class="h-20 left-0 bottom-0 w-screen">
-    <div class="bg-zinc-900 dark:bg-white fixed h-20 left-0 bottom-0 w-screen">
+<footer class="h-36 left-0 bottom-0 w-screen">
+    <div class="bg-zinc-900 dark:bg-white fixed h-36 left-0 bottom-0 w-screen">
+        <div>
+            <div class="flex justify-center items-center py-5 text-sm text-gray-400">
+                <a class="block px-2 py-1 hover:text-white dark:hover:text-black transition-colors" href="/impressum">impressum</a>
+                <div class="px-1">|</div>
+                <a class="block px-2 py-1 hover:text-white dark:hover:text-black transition-colors" href="/datenschutz">datenschutz</a
+                >
+            </div>
+        </div>
         <div class="text-sm text-center text-white dark:text-zinc-900 py-5">
             <b>© Karolin Rohmer {new Date().getFullYear()}</b><br/>
             Webdesign by <a href="https://matthias.rohmer.rocks" target="_blank">Matthias Rohmer</a>
