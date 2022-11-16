7fded7760fd50905937ebca77b3aa77c2fe41de6
mrohmer <rohmer.matze@gmail.com>
2022-11-16 20:49:31 +0100

    feat: add cloudflare analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import "../app.css";
   import Footer from "$lib/components/Footer.svelte";
+  import {dev} from '$app/environment';
+  import {PUBLIC_CLOUDFLARE_TOKEN} from '$env/static/public';
 </script>
 
 <svelte:head>
     <title>Karolin Rohmer</title>
+    {#if !dev && PUBLIC_CLOUDFLARE_TOKEN}
+        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon={JSON.stringify({"token": PUBLIC_CLOUDFLARE_TOKEN})}></script>
+    {/if}
 </svelte:head>
 
 <main class="z-10 min-h-screen bg-white dark:bg-zinc-900">
